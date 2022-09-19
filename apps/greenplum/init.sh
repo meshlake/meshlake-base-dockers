6f1515a882019e4a1f0a21148b7e5362a0376099
Ding <dwei@meshlake.com>
2022-09-19 17:58:53 +0800

    Introduce setup folder for greenplum

---
+++ apps/greenplum/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# whoami: gpadmin
+
+# only run on master
+host=`hostname`
+if [ $host = "gp-0" ];then
+   source /usr/local/greenplum-db/greenplum_path.sh
+   artifact/prepare.sh -s 2 -n 2
+   gpinitsystem -a -c gpinitsystem_config
+   source env.sh
+   artifact/postinstall.sh
+   gpstate -s
+fi
