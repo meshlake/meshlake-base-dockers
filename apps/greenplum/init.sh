daf959a58b5ceefc280d182d858441d549301d44
Ding <dwei@meshlake.com>
2022-09-20 14:27:11 +0800

    Introduce init.sh into greenplum build

--- apps/greenplum/init.sh
+++ apps/greenplum/init.sh
@@ -6,9 +6,9 @@
 host=`hostname`
 if [ $host = "${KUBERNETES_STATEFULSET_NAME:-greenplum}-0" ];then
    source /usr/local/greenplum-db/greenplum_path.sh
-   setup/prepare.sh -s 2 -n 2
+   ./setup/prepare.sh -s 2 -n 2
    gpinitsystem -a -c gpinitsystem_config
    source env.sh
-   setup/postinstall.sh
+   ./setup/postinstall.sh
    gpstate -s
 fi
