573f792401e2657bed15f6bc953a96087bbd8567
Ding <dwei@meshlake.com>
2022-09-20 17:34:24 +0800

    Update gpadmin into wheel group for sudoer

--- apps/greenplum/init.sh
+++ apps/greenplum/init.sh
@@ -6,7 +6,7 @@
 host=`hostname`
 if [ $host = "${KUBERNETES_STATEFULSET_NAME:-greenplum}-0" ];then
    source /usr/local/greenplum-db/greenplum_path.sh
-   ./setup/prepare.sh -s 2 -n 2
+   ./setup/prepare.sh -s 1 -n 1
    gpinitsystem -a -c gpinitsystem_config
    source env.sh
    ./setup/postinstall.sh
