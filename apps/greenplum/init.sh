399a412aad1f3f6c2156c6483e62ffc60cf1e9ab
Ding <dwei@meshlake.com>
2022-09-21 18:07:37 +0800

    Remove ssh-keygen on docker build

--- apps/greenplum/init.sh
+++ apps/greenplum/init.sh
@@ -12,10 +12,11 @@ fi
 # only run on master
 host=`hostname`
 if [ $host = "${KUBERNETES_STATEFULSET_NAME:-greenplum}-0" ];then
-   source /usr/local/greenplum-db/greenplum_path.sh
-   ./setup/prepare.sh -s 1 -n 1
-   gpinitsystem -a -c gpinitsystem_config
-   source env.sh
-   ./setup/postinstall.sh
-   gpstate -s
+    ssh-keygen -y -t rsa -b 4096 -f ${HOME}/.ssh/id_rsa -N ''
+    source /usr/local/greenplum-db/greenplum_path.sh
+    ./setup/prepare.sh -s 1 -n 1
+    gpinitsystem -a -c gpinitsystem_config
+    source env.sh
+    ./setup/postinstall.sh
+    gpstate -s
 fi
